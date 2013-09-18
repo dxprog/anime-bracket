b18e03402062e0293ed79ff113fb7575a679862b
Matt Hackmann <matt.hackmann@gmail.com>
2013-09-18 00:39:54 -0500

    Migrated to new Lib\Dal, removed redundant wording from properties, added getSimilarByName method

--- api/character.php
+++ api/character.php
@@ -4,16 +4,16 @@ namespace Api {
 	
 	use Lib;
 	
-	class Character extends Dal {
+	class Character extends Lib\Dal {
 	
 		/**
 		 * Object property to table column map
 		 */
 		protected $_dbMap = array(
-			'characterId' => 'character_id',
+			'id' => 'character_id',
 			'bracketId' => 'bracket_id',
-			'characterName' => 'character_name',
-			'characterSource' => 'character_source'
+			'name' => 'character_name',
+			'source' => 'character_source'
 		);
 		
 		/**
@@ -24,12 +24,12 @@ namespace Api {
 		/**
 		 * Primary key
 		 */
-		protected $_dbPrimaryKey = 'characterId';
+		protected $_dbPrimaryKey = 'id';
 	
 		/**
 		 * Character record ID
 		 */
-		public $characterId = 0;
+		public $id = 0;
 	
 		/**
 		 * Bracket ID
@@ -39,42 +39,65 @@ namespace Api {
 		/**
 		 * Character name
 		 */
-		public $characterName = 0;
+		public $name = 0;
 	
 		/**
 		 * Character source
 		 */
-		public $characterSource = 0;
+		public $source = 0;
 		
 		/**
 		 * Image path for this character
 		 */
-		public $characterImage = '';
+		public $image = '';
 		
 		/**
 		 * Constructor
 		 */
 		public function __construct($character = null) {
 			if (is_object($character)) {
-				$this->copyFromDbRow($character);
-				$this->characterImage = base_convert($this->characterId, 10, 36) . '.jpg';
+				parent::__construct($character);
+				$this->image = base_convert($this->id, 10, 36) . '.jpg';
 			}
 		}
-		
+
 		/**
-		 * Gets a character by ID
+		 * Override for image
 		 */
-		public static function getCharacterById($id) {
-			$retVal = false;
-			$params = array( 'characterId' => $id );
-			$result = Lib\Db::Query('SELECT * FROM `character` WHERE `character_id` = :characterId', $params);
-			if ($result && $result->count > 0) {
-				$row = Lib\Db::Fetch($result);
-				$retVal = new Character($row);
+		public function copyFromDbRow($row) {
+			parent::copyFromDbRow($row);
+			$this->image = base_convert($this->id, 10, 36) . '.jpg';
+		}
+
+		/**
+		 * Gets other nominees in this bracket with similar names (checks for Japanese and Western naming order)
+		 */
+		public static function getBySimilarName($name, $bracket) {
+			$retVal = null;
+			if ($bracket instanceof Bracket) {
+				$query = 'SELECT * FROM `character` WHERE bracket_id = :bracketId ';
+				$params = [ ':bracketId' => $bracket->id ];
+				$name = explode(' ', $name);
+				if (count($name) === 2) {
+					$params[':nameA'] = $name[0] . '%' . $name[1];
+					$params[':nameB'] = $name[1] . '%' . $name[0];
+					$query .= 'AND (character_name LIKE :nameA OR character_name LIKE :nameB)';
+				} else {
+					$params[':name'] = implode('%', $name);
+					$query .= 'AND character_name LIKE :name';
+				}
+
+				$result = Lib\Db::Query($query, $params);
+				if ($result && $result->count) {
+					$retVal = [];
+					while ($row = Lib\Db::Fetch($result)) {
+						$retVal[] = new Character($row);
+					}
+				}
 			}
 			return $retVal;
 		}
-	
+
 	}
 
 }
