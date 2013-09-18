1b845793c949a4df31ae84dc559f44d5982f2fbf
Matt Hackmann <matt.hackmann@gmail.com>
2013-09-18 00:59:13 -0500

    Reddit tokens, images directory path

--- config.php
+++ config.php
@@ -1,10 +1,21 @@
 <?php
 
 // Database credentials
-define ('DB_HOST', 'localhost');
-define ('DB_USER', '-USER-');
-define ('DB_PASS', '-PASSWORD-');
-define ('DB_NAME', 'anime_bracket');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'DB_USER');
+define('DB_PASS', 'DB_PASS');
+define('DB_NAME', 'anime_bracket');
+
+define('AWS_KEY', 'AWS_KEY');
+define('AWS_SECRET', 'AWS_SECRET');
+define('AWS_ENABLED', true);
+
+define('REDDIT_TOKEN', 'APP_TOKEN');
+define('REDDIT_SECRET', 'APP_SECRET');
+define('REDDIT_HANDLER', 'http://bracket.awwni.me/verify');
+define('REDDIT_MINAGE', 2592000);
+
+define('IMAGE_LOCATION', '/PATH/TO/IMAGES');
 
 // View directory
 define('VIEW_PATH', './view/');
