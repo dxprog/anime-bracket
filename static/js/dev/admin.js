1707dfe8709e76550c053b459873c1d059a073b3
Matt Hackmann <matt.hackmann@gmail.com>
2014-10-01 23:33:02 -0700

    image crop JS stuff (for reals)

--- static/js/dev/admin.js
+++ static/js/dev/admin.js
@@ -1,11 +1,21 @@
-(function() {
+(function(undefined) {
     
+    'use strict';
 
     var cropperInitialized = false,
         $cropper = null,
         imageSize = {},
 
+        UPLOAD_ENDPOINT = '/admin/upload/',
+        CROP_ENDPOINT = '/admin/crop/',
         CROPPER_PADDING = 3,
+        ACCEPTED_FORMATS = [
+            'image/jpeg',
+            'image/gif',
+            'image/png'
+        ],
+
+        cropCoords = {},
 
         /**
          * Multiple script loader
@@ -30,47 +40,147 @@
 
         changeImageClick = function(evt) {
 
-            var cropperOptions = {
-                    aspectRatio: 1,
-                    minSize: [ 150, 150 ]
-                },
-
-                center = 0;
-
             if (!cropperInitialized) {
-                if (imageSize) {
-                    if (imageSize.width < imageSize.height) {
-                        center = (imageSize.height - imageSize.width - CROPPER_PADDING * 2) / 2;
-                        center = center < 0 ? 0 : center;
-                        cropperOptions.setSelect = [ CROPPER_PADDING, center, imageSize.width - CROPPER_PADDING * 2, center + imageSize.width ];
-                    } else {
-
-                    }
-                }
-                $cropper.find('img').Jcrop(cropperOptions);
+                initCropper();
             }
 
             $cropper.fadeIn();
 
         },
 
-        getImageDimensions = function(url) {
-            var image = new Image;
+        initCropper = function() {
+
+            var cropperOptions = {
+                    aspectRatio: 1,
+                    minSize: [ 150, 150 ],
+                    onSelect: selectionUpdated
+                },
+
+                center = 0;
+
+            getImageDimensions().done(function(imageSize) {
+                if (imageSize.width < imageSize.height) {
+                    center = (imageSize.height - imageSize.width - CROPPER_PADDING * 2) / 2;
+                    center = center < 0 ? 0 : center;
+                    cropperOptions.setSelect = [ CROPPER_PADDING, center, imageSize.width - CROPPER_PADDING * 2, center + imageSize.width ];
+                } else {
+                    center = (imageSize.width - imageSize.height - CROPPER_PADDING * 2) / 2;
+                    center = center < 0 ? 0 : center;
+                    cropperOptions.setSelect = [ center, CROPPER_PADDING, center + imageSize.height, imageSize.height - CROPPER_PADDING * 2 ];
+                }
+                $cropper.find('img').Jcrop(cropperOptions);
+                cropperInitialized = true;
+            });
+        },
+
+        uploadComplete = function(data) {
+
+            if (data.success) {
+                cropperInitialized = false;
+                $cropper.find('.crop').empty().append('<img src="' + data.fileName + '" />');
+                initCropper();
+            } else {
+                alert(data.message);
+            }
+
+        },
+
+        selectionUpdated = function(coords) {
+            cropCoords = coords;
+        },
+
+        submitCrop = function(evt) {
+            alert('blah');
+            evt.preventDefault();
+
+            var imageFile = $cropper.find('img').attr('src');
+            $.ajax({
+                url: CROP_ENDPOINT,
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    imageFile: imageFile,
+                    x: cropCoords.x,
+                    y: cropCoords.y,
+                    width: cropCoords.w,
+                    height: cropCoords.h
+                }
+            }).done(function(data) {
+                if (data.success) {
+                    
+                } else {
+                    alert(data.message);
+                }
+            }).fail(function() {
+                alert('Unable to crop image');
+            });
+
+
+        },  
+
+        uploadChange = function(evt) {
+
+            var xhr = new XMLHttpRequest(),
+                files = evt.target.files,
+                file = '',
+                self = this,
+                formData = new FormData();
+
+            if (files.length > 0) {
+
+                file = files[0];
+
+                // Validate the type
+                if (ACCEPTED_FORMATS.indexOf(file.type) === -1) {
+                    alert('Image must be a JPEG, GIF, or PNG');
+                    return;
+                }
+
+                xhr.addEventListener('readystatechange', function() {
+                    if (xhr.readyState === 4) {
+                        try {
+                            uploadComplete(JSON.parse(xhr.responseText));
+                        } catch (e) {
+                            // do nothing because I hate error management. Someday...
+                        }
+                    }
+                });
+
+                xhr.open('POST', UPLOAD_ENDPOINT, true);
+                xhr.setRequestHeader('X-FileName', file.name);
+                formData.append('upload', file);
+                xhr.send(formData);
+
+            }
+
+        },
+
+        getImageDimensions = function() {
+            var image = new Image,
+                $dfr = $.Deferred(),
+                url = $cropper.find('img').attr('src');
             image.onload = function() {
-                imageSize.width = image.width;
-                imageSize.height = image.height;
+                $dfr.resolve({
+                    width: image.width,
+                    height: image.height
+                });
             };
             image.src = url;
+            return $dfr.promise();
+        },
+
+        initNomineeForm = function() {
+            loadScripts([ '/static/js/jquery.Jcrop.min.js' ]).done(function() {
+                $cropper = $('.overlay');
+                $('body')
+                    .on('click', '#changeImage', changeImageClick)
+                    .on('change', '[name="upload"]', uploadChange)
+                    .on('click', '.crop-submit', submitCrop);
+            });
         };
 
-    if ($('body#admin').length) {
-        loadScripts([ '/static/js/jquery.Jcrop.min.js' ]).done(function() {
-            
-            $cropper = $('.overlay');
-            getImageDimensions($cropper.find('img').attr('src'));
-            $('body').on('click', '#changeImage', changeImageClick);
-
-        });
+    if ($('body#admin .nominee').length) {
+        initNomineeForm();
     }
 
 }());
