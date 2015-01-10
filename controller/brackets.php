cba67a3bfccaf1b1a31516ff209b4d2d8189e2ba
Matt Hackmann <matt@dxprog.com>
2015-01-10 14:25:11 -0800

    current/past brackets

---
+++ controller/brackets.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Controller {
+
+    use Api;
+    use Lib;
+
+    class Brackets extends Page {
+
+        public static function generate(array $params) {
+
+            $active = array_shift($params) !== 'past';
+
+            $brackets = Lib\Cache::fetch(function() use ($active) {
+
+                $allBrackets = Api\Bracket::getAll();
+
+                // Filter out active/completed brackets
+                $brackets = [];
+                foreach ($allBrackets as $bracket) {
+                    if ($active && ($bracket->state == BS_ELIMINATIONS || $bracket->state == BS_VOTING || $bracket->state == BS_NOMINATIONS)) {
+                        $brackets[] = $bracket;
+                    }
+
+                    if (!$active && $bracket->state == BS_FINAL) {
+                        $brackets[] = $bracket;
+                    }
+                }
+
+                // Check for card images
+                foreach ($brackets as $bracket) {
+                    if (is_readable('./images/bracket_' . $bracket->id . '_card.jpg')) {
+                        $bracket->cardImage = '/images/bracket_' . $bracket->id . '_card.jpg';
+                    } else {
+                        $bracket->entrants = Api\Character::getRandomCharacters($bracket, 9);
+                    }
+                }
+
+                // Sort the brackets by reverse date
+                usort($brackets, function($a, $b) {
+                    return $a->start > $b->start ? -1 : 1;
+                });
+
+                return $brackets;
+
+            }, 'Controller::Brackets_displayBrackets_' . ($active ? 'active' : 'completed'));
+
+            Lib\Display::addKey('page', 'brackets');
+            Lib\Display::renderAndAddKey('content', 'bracketsView', $brackets);
+
+        }
+
+    }
+
+}
