7b16769d4d2d1efd271ddbe9f0d37ee255963259
Matt Hackmann <matt@dxprog.com>
2015-01-24 16:32:28 -0800

    separation of individual vs core config values

---
+++ app-config.sample.php
@@ -0,0 +1,19 @@
+<?php
+
+define('BRACKET_SOURCE', 1);
+
+define('REDDIT_TOKEN', 'reddit_token');
+define('REDDIT_SECRET', 'reddit_secret');
+define('REDDIT_HANDLER', 'http://reddit_oauth_handler');
+define('REDDIT_MINAGE', 2592000);
+
+define('IMAGE_LOCATION', '/path/to/images');
+define('IMAGE_URL', 'http://url.to/images');
+
+define('REDIS_SERVER', 'tcp://127.0.0.1:6379');
+define('CACHE_PREFIX', 'AnimeBracket');
+
+define('CSS_VERSION', '20140816');
+define('JS_VERSION', '20131109');
+
+define('SESSION_DOMAIN', '.brakk.it');
