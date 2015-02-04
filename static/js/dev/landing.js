9b34521f944a594b31b299d75c8c84a08c3f011d
Matt Hackmann <matt@dxprog.com>
2015-02-03 18:40:52 -0800

    fixed crashing on small screens

--- static/js/dev/landing.js
+++ static/js/dev/landing.js
@@ -6,6 +6,7 @@
         LEFT_PADDING = 65,
         FORWARD = 'forward',
         BACKSLASH = 'backslash',
+        BAIL_TRIES = 10,
 
         data = window.rounds || [],
 
@@ -18,37 +19,43 @@
         getRandomPosition = function() {
 
             var x = Math.round(Math.random() * cols),
-                y = Math.round(Math.random() * rows);
+                y = Math.round(Math.random() * rows),
+                bail = 0;
 
-            while (typeof grid[y * rows + x] === 'object') {
+            while (typeof grid[y * rows + x] === 'object' && bail < BAIL_TRIES) {
                 x = Math.round(Math.random() * cols);
                 y = Math.round(Math.random() * rows);
+                bail++;
             }
 
-            return y * rows + x;
+            return bail < BAIL_TRIES ? y * rows + x : null;
 
         },
 
         placeHex = function(round) {
             var index = getRandomPosition(),
-                y = Math.floor(index / cols),
-                x = index - (y * cols),
+                y = index ? Math.floor(index / cols) : null,
+                x = index ? index - (y * cols) : null,
                 direction = Math.random() > 0.5 ? BACKSLASH : FORWARD,
-                xOffset = direction === FORWARD ? -(HEX_WIDTH / 2) : 0;
+                xOffset = direction === FORWARD ? -(HEX_WIDTH / 2) : 0,
+                retVal = '';
 
-            xOffset += y % 2 > 0 ? HEX_WIDTH / 2 : 0;
+            if (null !== index) {
+                xOffset += y % 2 > 0 ? HEX_WIDTH / 2 : 0;
 
-            grid[(y + 1) * cols + (y % 2 > 0 ? -1 : 0) + x] = {};
-            grid[index] = {
-                character1: round.character1,
-                character2: round.character2,
-                x: x * HEX_WIDTH + LEFT_PADDING + xOffset,
-                y: y * HEX_HEIGHT + TOP_PADDING,
-                direction: direction,
-                fadeDelay: itr++
-            };
+                grid[(y + 1) * cols + (y % 2 > 0 ? -1 : 0) + x] = {};
+                grid[index] = {
+                    character1: round.character1,
+                    character2: round.character2,
+                    x: x * HEX_WIDTH + LEFT_PADDING + xOffset,
+                    y: y * HEX_HEIGHT + TOP_PADDING,
+                    direction: direction,
+                    fadeDelay: itr++
+                };
+                retVal = Templates['views/hexRound'](grid[index]);
+            }
 
-            return Templates['views/hexRound'](grid[index]);
+            return retVal;
 
         },
 
