c8542dbbb79810f34c1f8e1b69d79f7ea60967a7
Matt Hackmann <matt@dxprog.com>
2015-02-03 19:31:05 -0800

    fix warning when no brackets advance

--- cron/advance.php
+++ cron/advance.php
@@ -9,11 +9,13 @@ Lib\Cache::setDisabled(true);
 // Get all brackets that need to be updated
 $brackets = Api\Bracket::queryReturnAll([ 'advanceHour' => gmdate('G'), 'state' => [ 'in' => [ BS_ELIMINATIONS, BS_VOTING ] ] ]);
 
-foreach ($brackets as $bracket) {
-    echo 'Advancing ', $bracket->name, '...';
-    $bracket->advance();
-    Api\Bracket::getBracketByPerma($bracket->perma);
-    echo 'DONE', PHP_EOL;
+if ($brackets && count($brackets)) {
+    foreach ($brackets as $bracket) {
+        echo 'Advancing ', $bracket->name, '...';
+        $bracket->advance();
+        Api\Bracket::getBracketByPerma($bracket->perma);
+        echo 'DONE', PHP_EOL;
+    }
 }
 
 // Refresh caches
