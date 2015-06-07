27a59bd3de56252ab798042b89fca12ab7731cec
Matt Hackmann <matt@dxprog.com>
2015-06-07 10:09:47 -0700

    5 minute delay between bracket advances

--- controller/admin/advance.php
+++ controller/admin/advance.php
@@ -8,17 +8,31 @@ namespace Controller\Admin {
 
     class Advance extends \Controller\Me {
 
+        // The amount of time in seconds to delay bracket advancing
+        const BRACKET_ADVANCE_DELAY = 300;
+
         public static function generate(array $params) {
 
             $message = null;
             $bracket = self::_getBracket(array_shift($params));
             if ($bracket) {
-                $bracket->advance();
-                $message = new stdClass;
-                $message->type = 'success';
-                $message->message = $bracket->name . ' has advanced to the next round';
 
-                self::_refreshCaches($bracket);
+                $cacheKey = 'Controller::Admin::Advance_bracketAdvanceTime_' . $bracket->id;
+                $lastBracketAdvance = Lib\Cache::Get($cacheKey);
+                if (!$lastBracketAdvance || $lastBracketAdvance + self::BRACKET_ADVANCE_DELAY < time()) {
+                    $bracket->advance();
+                    $message = new stdClass;
+                    $message->type = 'success';
+                    $message->message = $bracket->name . ' has advanced to the next round';
+                    Lib\Cache::Set($cacheKey, time());
+                    self::_refreshCaches($bracket);
+                } else {
+                    $message = new stdClass;
+                    $message->type = 'error';
+                    $delta = $lastBracketAdvance + self::BRACKET_ADVANCE_DELAY - time();
+                    $time = Lib\Util::relativeTime(time() - $delta);
+                    $message->message = $bracket->name . ' was recently advanced. Please wait ' . $time . ' before advancing again.';
+                }
 
             }
 
