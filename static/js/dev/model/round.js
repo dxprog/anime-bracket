75cfd1963c83da518e1f071c652ed9e83f06392b
Matt Hackmann <matt@dxprog.com>
2016-01-13 14:14:24 -0800

    fixed 'pending' label on finalized rounds that had no votes

--- static/js/dev/model/round.js
+++ static/js/dev/model/round.js
@@ -4,11 +4,12 @@ export default function Round(data) {
   this.tier = parseInt(data.tier, 10);
   this.group = parseInt(data.group, 10);
   this.order = parseInt(data.order, 10);
+  this.final = !!data.final;
   if(data.character1) {
     this.entrants = 1;
     this.entrant1 = new Entrant(data.character1, 'top');
     this.id = data.id;
-    
+
     // Character ID is reserved for "nobody", used in wildcard and eliminations
     if (null !== data.character2 && data.character2.id !== 1) {
       this.entrant2 = new Entrant(data.character2, 'bottom');
