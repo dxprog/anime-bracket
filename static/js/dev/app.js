f04dbdaa7214bfdf6842d1915e3c1f85c07f6524
Matt Hackmann <matt@dxprog.com>
2016-01-17 13:07:52 -0800

    characters page migrated

--- static/js/dev/app.js
+++ static/js/dev/app.js
@@ -5,12 +5,14 @@ import Landing from './views/landing';
 import BracketDisplay from './views/bracket-display';
 import Voting from './views/voting';
 import Nominations from './views/nominations';
+import Characters from './views/characters';
 
 Router.addRoutes({
   'index': Landing,
   '/results/:perma/': BracketDisplay,
   '/vote/:perma/': Voting,
-  '/nominate/:perma/': Nominations
+  '/nominate/:perma/': Nominations,
+  '/characters/:perma/': Characters
 });
 
 Router.go(window.location.pathname);
---
+++ static/js/dev/views/characters.js
@@ -0,0 +1,90 @@
+import $ from 'jquery';
+
+import Route from '../lib/route';
+
+import characterList from '../../../../views/partials/_characterList.hbs';
+
+export default Route('characters', {
+
+  initSortData() {
+
+    const characters = this._characters;
+
+    let sourceSort = this._sourceSort = {};
+    let seededSort = this._seededSort = new Array(characters.length);
+    let nonSeedItr = characters.length;
+    let maxSeed = 0;
+
+    characters.forEach((character) => {
+
+      if (!sourceSort.hasOwnProperty(character.source)) {
+        sourceSort[character.source] = [];
+      }
+      sourceSort[character.source].push(character)
+
+      if (character.seed) {
+        seededSort[character.seed - 1] = character;
+        maxSeed = character.seed > maxSeed ? character.seed : maxSeed;
+      } else {
+        // Push non-seeded characters onto the back of the stack
+        seededSort[--nonSeedItr] = character;
+      }
+
+    });
+
+    this._maxSeed = maxSeed;
+
+  },
+
+  resortEntrants(evt) {
+    const seededSort = this._seededSort;
+    const sourceSort = this._sourceSort;
+    const maxSeed = this._maxSeed;
+
+    let sortBy = $(evt.currentTarget).val();
+    let dataSets = [];
+
+    switch (sortBy) {
+      case 'seed':
+        dataSets.push({
+          header: 'Seeded Entrants',
+          characters: seededSort.slice(0, maxSeed - 1)
+        });
+        dataSets.push({
+          header: 'Eliminated Entrants',
+          characters: seededSort.slice(maxSeed)
+        });
+        break;
+      case 'source':
+        for (var source in sourceSort) {
+          if (!!sourceSort[source]) {
+            dataSets.push({
+              header: source,
+              characters: sourceSort[source]
+            });
+          }
+        }
+        break;
+      default:
+        dataSets.push({
+          characters: this._characters
+        });
+        break;
+    }
+
+    var out = '';
+    dataSets.forEach(function(item) {
+      out += characterList(item);
+    });
+
+    this._$roster.html(out);
+  },
+
+  initRoute() {
+    this._characters = window._characters;
+    this.initSortData();
+    this._$roster = $('#roster');
+    $('[name="sort"]').on('change', this.resortEntrants.bind(this));
+  }
+
+});
