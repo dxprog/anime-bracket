1ba17693e492964555b8c0b3542117b22cee5bcb
Matt Hackmann <matt@dxprog.com>
2016-01-22 15:55:29 -0800

    moved actual nominee/stats code over

--- static/js/dev/views/admin/nominee.js
+++ static/js/dev/views/admin/nominee.js
@@ -1,2 +1,230 @@
-/* anime-bracket 2016-01-10 */
-!function(){var a=!1,b=null,c="checked",d="/me/image/upload/",e="/me/image/crop/";CROPPER_PADDING=3,ACCEPTED_FORMATS=["image/jpeg","image/gif","image/png"],cropCoords={},changeImageClick=function(){a||initCropper(),b.fadeIn()},displayMessage=function(a,b){$(".message").html(a).removeClass("hidden error success").addClass(b?"success":"error"),$(window).scrollTop(0)},initCropper=function(){var c={aspectRatio:1,minSize:[150,150],onSelect:selectionUpdated},d=0;getImageDimensions().done(function(e){e.width<e.height?(d=(e.height-e.width-2*CROPPER_PADDING)/2,d=0>d?0:d,c.setSelect=[CROPPER_PADDING,d,e.width-2*CROPPER_PADDING,d+e.width]):(d=(e.width-e.height-2*CROPPER_PADDING)/2,d=0>d?0:d,c.setSelect=[d,CROPPER_PADDING,d+e.height,e.height-2*CROPPER_PADDING]),b.find("img").Jcrop(c),a=!0})},uploadComplete=function(c){c.success?(a=!1,b.find(".crop").empty().append('<img src="'+c.fileName+'" />'),initCropper()):alert(c.message)},selectionUpdated=function(a){cropCoords=a},submitNominee=function(a){a.preventDefault();var b=$("form"),c=b.serialize(),d=$(a.target).val()?"ignore=true":"";d&&(d=(c.length?"&":"")+d,c+=d),$.ajax({url:b.attr("action"),data:c,dataType:"json",type:"POST"}).done(function(a){a.success?($("#content").html(Templates["views/admin/nominee"](a)),renderComplete(),displayMessage(a.message,!0)):displayMessage(a.message,!1)}).fail(function(){displayMessage("There was an error sending to the server",!1)})},ignoreCheck=function(a){var b=$(a.target).closest("tr");a.target.checked?(b.addClass(c),b.find("label.button").text("Not Duplicate")):(b.removeClass(c),b.find("label.button").text("Is Duplicate"))},submitCrop=function(a){a.preventDefault();var c=b.find("img").attr("src");$.ajax({url:e,type:"POST",dataType:"json",data:{imageFile:c,x:cropCoords.x,y:cropCoords.y,width:cropCoords.w,height:cropCoords.h}}).done(function(a){a.success?($(".nominee .image img").attr("src",a.fileName),$('[name="imageFile"]').val(a.fileName),b.fadeOut()):alert(a.message)}).fail(function(){alert("Unable to crop image")})},uploadChange=function(a){var b=new XMLHttpRequest,c=a.target.files,e="",f=new FormData;if(c.length>0){if(e=c[0],-1===ACCEPTED_FORMATS.indexOf(e.type))return void alert("Image must be a JPEG, GIF, or PNG");b.addEventListener("readystatechange",function(){if(4===b.readyState)try{uploadComplete(JSON.parse(b.responseText))}catch(a){}}),b.open("POST",d,!0),b.setRequestHeader("X-FileName",e.name),f.append("upload",e),b.send(f)}},copyCharacterClick=function(a){var b=$(a.currentTarget);$('[name="name"]').val(b.data("name")),$('[name="source"]').val(b.data("source")),$('[name="imageFile"]').val(b.data("image")),$(".image img").attr("src",b.data("image"))},getImageDimensions=function(){var a=new Image,c=$.Deferred(),d=b.find("img").attr("src");return a.onload=function(){c.resolve({width:a.width,height:a.height})},a.src=d,c.promise()},renderComplete=function(){b=$(".overlay"),a=!1},init=function(){$("body").on("click","#changeImage",changeImageClick).on("change",'[name="upload"]',uploadChange).on("click",".crop-submit",submitCrop).on("change",'input[type="checkbox"]',ignoreCheck).on("click",".buttons button",submitNominee).on("click","button.copy",copyCharacterClick).on("click","button.same",submitNominee),renderComplete()},init()}();
+(function() {
+
+    var cropperInitialized = false,
+        $cropper = null,
+        imageSize = {},
+
+        CHECKED = 'checked',
+        UPLOAD_ENDPOINT = '/me/image/upload/',
+        CROP_ENDPOINT = '/me/image/crop/',
+        SUBMIT_ENDPOINT = '/me/process/nominee/'
+        CROPPER_PADDING = 3,
+        ACCEPTED_FORMATS = [
+            'image/jpeg',
+            'image/gif',
+            'image/png'
+        ],
+
+        cropCoords = {},
+
+        changeImageClick = function(evt) {
+
+            if (!cropperInitialized) {
+                initCropper();
+            }
+
+            $cropper.fadeIn();
+
+        },
+
+        displayMessage = function(message, success) {
+            $('.message').html(message).removeClass('hidden error success').addClass(success ? 'success' : 'error');
+            $(window).scrollTop(0);
+        },
+
+        initCropper = function() {
+
+            var cropperOptions = {
+                    aspectRatio: 1,
+                    minSize: [ 150, 150 ],
+                    onSelect: selectionUpdated
+                },
+
+                center = 0;
+
+            getImageDimensions().done(function(imageSize) {
+                if (imageSize.width < imageSize.height) {
+                    center = (imageSize.height - imageSize.width - CROPPER_PADDING * 2) / 2;
+                    center = center < 0 ? 0 : center;
+                    cropperOptions.setSelect = [ CROPPER_PADDING, center, imageSize.width - CROPPER_PADDING * 2, center + imageSize.width ];
+                } else {
+                    center = (imageSize.width - imageSize.height - CROPPER_PADDING * 2) / 2;
+                    center = center < 0 ? 0 : center;
+                    cropperOptions.setSelect = [ center, CROPPER_PADDING, center + imageSize.height, imageSize.height - CROPPER_PADDING * 2 ];
+                }
+                $cropper.find('img').Jcrop(cropperOptions);
+                cropperInitialized = true;
+            });
+        },
+
+        uploadComplete = function(data) {
+
+            if (data.success) {
+                cropperInitialized = false;
+                $cropper.find('.crop').empty().append('<img src="' + data.fileName + '" />');
+                initCropper();
+            } else {
+                alert(data.message);
+            }
+
+        },
+
+        selectionUpdated = function(coords) {
+            cropCoords = coords;
+        },
+
+        submitNominee = function(evt) {
+            evt.preventDefault();
+
+            var $form = $('form'),
+                data = $form.serialize(),
+                ignore = $(evt.target).val() ? 'ignore=true' : '';
+
+            if (ignore) {
+                ignore = (data.length ? '&' : '') + ignore;
+                data += ignore;
+            }
+
+            $.ajax({
+                url: $form.attr('action'),
+                data: data,
+                dataType: 'json',
+                type: 'POST'
+            }).done(function(data) {
+
+                if (data.success) {
+                    $('#content').html(Templates['views/admin/nominee'](data));
+                    renderComplete();
+                    displayMessage(data.message, true);
+                } else {
+                    displayMessage(data.message, false);
+                }
+
+            }).fail(function() {
+                displayMessage('There was an error sending to the server', false);
+            });
+
+        },
+
+        ignoreCheck = function(evt) {
+            var $parent = $(evt.target).closest('tr');
+            if (evt.target.checked) {
+                $parent.addClass(CHECKED);
+                $parent.find('label.button').text('Not Duplicate');
+            } else {
+                $parent.removeClass(CHECKED);
+                $parent.find('label.button').text('Is Duplicate');
+            }
+        },
+
+        submitCrop = function(evt) {
+
+            evt.preventDefault();
+
+            var imageFile = $cropper.find('img').attr('src');
+            $.ajax({
+                url: CROP_ENDPOINT,
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    imageFile: imageFile,
+                    x: cropCoords.x,
+                    y: cropCoords.y,
+                    width: cropCoords.w,
+                    height: cropCoords.h
+                }
+            }).done(function(data) {
+                if (data.success) {
+                    // Replace the old image
+                    $('.nominee .image img').attr('src', data.fileName);
+                    $('[name="imageFile"]').val(data.fileName);
+                    $cropper.fadeOut();
+                } else {
+                    alert(data.message);
+                }
+            }).fail(function() {
+                alert('Unable to crop image');
+            });
+
+
+        },
+
+        uploadChange = function(evt) {
+
+            var xhr = new XMLHttpRequest(),
+                files = evt.target.files,
+                file = '',
+                self = this,
+                formData = new FormData();
+
+            if (files.length > 0) {
+
+                file = files[0];
+
+                // Validate the type
+                if (ACCEPTED_FORMATS.indexOf(file.type) === -1) {
+                    alert('Image must be a JPEG, GIF, or PNG');
+                    return;
+                }
+
+                xhr.addEventListener('readystatechange', function() {
+                    if (xhr.readyState === 4) {
+                        try {
+                            uploadComplete(JSON.parse(xhr.responseText));
+                        } catch (e) {
+                            // do nothing because I hate error management. Someday...
+                        }
+                    }
+                });
+
+                xhr.open('POST', UPLOAD_ENDPOINT, true);
+                xhr.setRequestHeader('X-FileName', file.name);
+                formData.append('upload', file);
+                xhr.send(formData);
+
+            }
+
+        },
+
+        copyCharacterClick = function(evt) {
+            var $target = $(evt.currentTarget);
+            $('[name="name"]').val($target.data('name'));
+            $('[name="source"]').val($target.data('source'));
+            $('[name="imageFile"]').val($target.data('image'));
+            $('.image img').attr('src', $target.data('image'));
+        },
+
+        getImageDimensions = function() {
+            var image = new Image,
+                $dfr = $.Deferred(),
+                url = $cropper.find('img').attr('src');
+            image.onload = function() {
+                $dfr.resolve({
+                    width: image.width,
+                    height: image.height
+                });
+            };
+            image.src = url;
+            return $dfr.promise();
+        },
+
+        renderComplete = function() {
+            $cropper = $('.overlay');
+            cropperInitialized = false;
+        },
+
+        init = function() {
+            $('body')
+                .on('click', '#changeImage', changeImageClick)
+                .on('change', '[name="upload"]', uploadChange)
+                .on('click', '.crop-submit', submitCrop)
+                .on('change', 'input[type="checkbox"]', ignoreCheck)
+                .on('click', '.buttons button', submitNominee)
+                .on('click', 'button.copy', copyCharacterClick)
+                .on('click', 'button.same', submitNominee);
+            renderComplete();
+        };
+
+    init();
+
+}());
--- static/js/dev/views/admin/stats.js
+++ static/js/dev/views/admin/stats.js
@@ -1,2 +1,65 @@
-/* anime-bracket 2016-01-10 */
-!function(){var a=null,b=null,c=null,d=function(a){var b=[],c=[],d=[];return a.forEach(function(a){b.push("Round "+(a.tier+1)+", Group "+String.fromCharCode(a.group+65)),c.push(a.total),d.push(a.userTotal)}),{labels:b,totalVotes:c,userCount:d}},e=function(){var e={};window.statsData&&(e=d(window.statsData),console.log(e),e.datasets=[{label:"Users",fillColor:"rgba(220,220,220,0.2)",strokeColor:"rgba(220,220,220,1)",pointColor:"rgba(220,220,220,1)",pointStrokeColor:"#fff",pointHighlightFill:"#fff",pointHighlightStroke:"rgba(220,220,220,1)",data:e.userCount},{label:"Votes",fillColor:"rgba(151,187,205,0.2)",strokeColor:"rgba(151,187,205,1)",pointColor:"rgba(151,187,205,1)",pointStrokeColor:"#fff",pointHighlightFill:"#fff",pointHighlightStroke:"rgba(151,187,205,1)",data:e.totalVotes}],console.log(e),a=document.getElementById("chart"),a.setAttribute("width",$("#content").width()),b=a.getContext("2d"),c=new Chart(b).Line(e))};$(e)}();
+(function(undefined) {
+
+    var canvas = null,
+        ctx = null,
+        chart = null,
+
+        getChartData = function(data) {
+            var labels = [],
+                totalVotes = [],
+                userCount = [];
+
+            data.forEach(function(item) {
+                labels.push('Round ' + (item.tier + 1) + ', Group ' + String.fromCharCode(item.group + 65));
+                totalVotes.push(item.total);
+                userCount.push(item.userTotal);
+            });
+
+            return {
+                labels: labels,
+                totalVotes: totalVotes,
+                userCount: userCount
+            };
+        },
+
+        init = function() {
+
+            var data = {};
+
+            if (window.statsData) {
+                data = getChartData(window.statsData);
+                console.log(data);
+                data.datasets = [
+                    {
+                        label: 'Users',
+                        fillColor: 'rgba(220,220,220,0.2)',
+                        strokeColor: 'rgba(220,220,220,1)',
+                        pointColor: 'rgba(220,220,220,1)',
+                        pointStrokeColor: '#fff',
+                        pointHighlightFill: '#fff',
+                        pointHighlightStroke: 'rgba(220,220,220,1)',
+                        data: data.userCount
+                    },
+                    {
+                        label: 'Votes',
+                        fillColor: 'rgba(151,187,205,0.2)',
+                        strokeColor: 'rgba(151,187,205,1)',
+                        pointColor: 'rgba(151,187,205,1)',
+                        pointStrokeColor: '#fff',
+                        pointHighlightFill: '#fff',
+                        pointHighlightStroke: 'rgba(151,187,205,1)',
+                        data: data.totalVotes
+                    }
+                ];
+console.log(data);
+                canvas = document.getElementById('chart');
+                canvas.setAttribute('width', $('#content').width());
+                ctx = canvas.getContext('2d');
+                chart = new Chart(ctx).Line(data);
+            }
+
+        };
+
+    $(init);
+
+}());
