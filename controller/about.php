490356d4e8029f6fab71241a7b6e46a1b8715f12
Matt Hackmann <matt@dxprog.com>
2016-02-21 12:18:54 -0800

    fixed up static content pages

--- controller/about.php
+++ controller/about.php
@@ -10,11 +10,25 @@ namespace Controller {
             $page = array_shift($params);
             $page = $page ?: 'about';
             $path = VIEW_PATH . '/' . $page . '.md';
-            $out = 'Uh oh... that doesn\'t seem to exist...';
+            $out = (object) [
+              'content' => 'Uh oh... that doesn\'t seem to exist...'
+            ];
 
             if (is_readable($path)) {
                 $file = file_get_contents($path);
-                $out = Lib\Michelf\Markdown::defaultTransform($file);
+
+                // Extract out a page header
+                if (preg_match('/^##[\w]/', $file)) {
+                  $lines = explode(PHP_EOL, $file);
+                  $header = substr(array_shift($lines), 2);
+                  // Browser page title
+                  Lib\Display::addKey('title', $header . DEFAULT_TITLE_SUFFIX);
+                  $out->header = $header;
+                  $file = implode(PHP_EOL, $lines);
+                  unset($lines);
+                }
+
+                $out->content = Lib\Michelf\Markdown::defaultTransform($file);
             }
 
             Lib\Display::addKey('page', 'static');
