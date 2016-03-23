2156eb31f5fc45aa9e539b48579148fead4d7b54
Matt Hackmann <matt@dxprog.com>
2016-03-23 00:41:22 -0700

    db does not connect in testing. also, adding predis to composer

--- config.sample.php
+++ config.sample.php
@@ -1,6 +1,7 @@
 <?php
 
 // Database credentials
+define('DB_DISABLE', true); // Remove this for production
 define('DB_HOST', 'localhost');
 define('DB_USER', 'username');
 define('DB_PASS', 'password');
