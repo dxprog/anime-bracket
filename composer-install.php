48b91ca505217c5d8e244c2ab5b10666dd17fe31
Matt Hackmann <matt@dxprog.com>
2016-03-22 23:22:23 -0700

    attempt to get composer installer due to single lang limit

---
+++ composer-install.php
@@ -0,0 +1,10 @@
+<?php
+
+$installer = file_get_contents('https://getcomposer.org/installer');
+if (hash('SHA384', $installer) === '41e71d86b40f28e771d4bb662b997f79625196afcca95a5abf44391188c695c6c1456e16154c75a211d238cc3bc5cb47') {
+  echo 'Installer verified';
+  $installer = str_replace([ '<?php', '?>' ], '', $installer);
+  eval($installer);
+} else {
+  echo 'Installer corrupt';
+}
