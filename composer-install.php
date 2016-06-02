55120a8aa781f8faa948b3afeedc1a95daa47a8e
Matt Hackmann <matt@dxprog.com>
2016-06-01 22:52:30 -0700

    updated composer hash

--- composer-install.php
+++ composer-install.php
@@ -1,7 +1,7 @@
 <?php
 
 $installer = file_get_contents('https://getcomposer.org/installer');
-if (hash('SHA384', $installer) === 'a52be7b8724e47499b039d53415953cc3d5b459b9d9c0308301f867921c19efc623b81dfef8fc2be194a5cf56945d223') {
+if (hash('SHA384', $installer) === '070854512ef404f16bac87071a6db9fd9721da1684cd4589b1196c3faf71b9a2682e2311b36a5079825e155ac7ce150d') {
   echo 'Installer verified';
   $installer = str_replace(array('<?php', '?>'), '', $installer);
   eval($installer);
