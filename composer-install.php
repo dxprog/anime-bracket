3b61938eae75273c378bcf44efad70697c3eca0d
Matt Hackmann <matt@dxprog.com>
2018-08-04 22:25:22 -0700

    fixed composer installer... forever

--- composer-install.php
+++ composer-install.php
@@ -1,7 +1,8 @@
 <?php
 
+$expectedSignature = trim(file_get_contents('https://composer.github.io/installer.sig'));
 $installer = file_get_contents('https://getcomposer.org/installer');
-if (hash('SHA384', $installer) === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') {
+if (hash('SHA384', $installer) === $expectedSignature) {
   echo 'Installer verified';
   $installer = str_replace(array('<?php', '?>'), '', $installer);
   eval($installer);
