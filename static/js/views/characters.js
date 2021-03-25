82f7864e9ec6cde517895e5b7042bbc1bf8cbf35
Matt Hackmann <matt@dxprog.com>
2021-03-24 22:49:21 -0700

    cleanup

--- static/js/views/characters.js
+++ static/js/views/characters.js
@@ -37,13 +37,12 @@ const sortEntrantsBySource = entrants => {
       entrants: sources[source].sort((a, b) => a.name < b.name ? -1 : 1),
     }
   )).sort((a, b) => a.title < b.title ? -1 : 1);
-}
+};
 
 const EntrantsView = ({ bracket, characters }) => {
   const [ entrantsList, setEntrantsList ] = useState([]);
   const [ sortKey, setSortKey ] = useState(SortKeys.Name);
 
-  // Initial population of the entrants list, sorted by name
   useEffect(() => {
     switch (sortKey) {
       case SortKeys.Source:
@@ -54,7 +53,6 @@ const EntrantsView = ({ bracket, characters }) => {
         setEntrantsList(sortEntrantsByName(characters));
         break;
     }
-    console.log('changed', sortKey);
   }, [sortKey]);
 
   const hasSeededEntrants = bracket.state === BracketStates.Voting || bracket.state === BracketStates.Final;
@@ -120,91 +118,13 @@ EntrantsView.propTypes = {
 };
 
 export default Route('characters', {
-
-  initSortData() {
-
-    const characters = this._characters;
-
-    let sourceSort = this._sourceSort = {};
-    let seededSort = this._seededSort = new Array(characters.length);
-    let nonSeedItr = characters.length;
-    let maxSeed = 0;
-
-    characters.forEach((character) => {
-
-      if (!sourceSort.hasOwnProperty(character.source)) {
-        sourceSort[character.source] = [];
-      }
-      sourceSort[character.source].push(character)
-
-      if (character.seed) {
-        seededSort[character.seed - 1] = character;
-        maxSeed = character.seed > maxSeed ? character.seed : maxSeed;
-      } else {
-        // Push non-seeded characters onto the back of the stack
-        seededSort[--nonSeedItr] = character;
-      }
-
-    });
-
-    this._maxSeed = maxSeed;
-
-  },
-
-  resortEntrants(evt) {
-    const seededSort = this._seededSort;
-    const sourceSort = this._sourceSort;
-    const maxSeed = this._maxSeed;
-
-    let sortBy = $(evt.currentTarget).val();
-    let dataSets = [];
-
-    switch (sortBy) {
-      case 'seed':
-        dataSets.push({
-          header: 'Seeded Entrants',
-          characters: seededSort.slice(0, maxSeed)
-        });
-        dataSets.push({
-          header: 'Eliminated Entrants',
-          characters: seededSort.slice(maxSeed)
-        });
-        break;
-      case 'source':
-        for (var source in sourceSort) {
-          if (!!sourceSort[source]) {
-            dataSets.push({
-              header: source,
-              characters: sourceSort[source]
-            });
-          }
-        }
-        break;
-      default:
-        dataSets.push({
-          characters: this._characters
-        });
-        break;
-    }
-
-    var out = '';
-    dataSets.forEach(function(item) {
-      out += characterList(item);
-    });
-
-    this._$roster.html(out);
-  },
-
   initRoute() {
-    this._characters = window._characters;
-    // this.initSortData();
-    // this._$roster = $('#roster');
-    // $('[name="sort"]').on('change', this.resortEntrants.bind(this));
-    // Handlebars.registerHelper('metaLabel', this.metaLabelHelper.bind(this));
     ReactDOM.render(
-      <EntrantsView characters={window.bootstrap.characters} bracket={window.bootstrap.bracket} />,
+      <EntrantsView
+        characters={window.bootstrap.characters}
+        bracket={window.bootstrap.bracket}
+      />,
       document.getElementById('roster')
     );
   }
-
 });
