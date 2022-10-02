8c858ce6e1b1862d0d26ef9f5a5c3d9447bc251d
Matt Hackmann <matt.hackmann@gmail.com>
2022-10-01 22:23:14 -0700

    update to webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,71 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './static/js/app.js',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: {
-          loader: 'babel-loader'
+module.exports = (env, argv) => {
+  const isDev = argv.mode !== 'production';
+  const plugins = [];
+
+  if (!isDev) {
+    plugins.push(new MinifyPlugin());
+  }
+
+  return {
+    mode: isDev ? 'development' : 'production',
+    entry: './static/js/app.js',
+    devtool: isDev ? 'inline-source-map' : 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
         },
-        exclude: /node_modules/
-      },
-      {
-        test: /\.hbs$/,
-        loader: 'handlebars-loader',
-        query: {
-          partialResolver(partial, callback) {
-            callback(null, path.resolve(__dirname, 'views/partials', `_${partial}.hbs`));
+        {
+          test: /\.hbs$/,
+          loader: 'handlebars-loader',
+          options: {
+            partialResolver(partial, callback) {
+              callback(null, path.resolve(__dirname, 'views/partials', `_${partial}.hbs`));
+            },
+            precompileOptions: {
+              knownHelpersOnly: false,
+            },
           },
-          precompileOptions: {
-            knownHelpersOnly: false
-          }
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: [ '.js' ],
-    alias: {
-      '@views': path.resolve(__dirname, 'views'),
-      '@scss': path.resolve(__dirname, 'static/scss'),
-      '@src': path.resolve(__dirname, 'static/js'),
-    }
-  },
-  output: {
-    filename: 'static/brakkit.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'static/brakkit.css'
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: path.resolve(__dirname, 'static/images/'), to: 'static/images/' }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
       ],
-    })
-  ]
+    },
+    resolve: {
+      extensions: [ '.js' ],
+      alias: {
+        '@views': path.resolve(__dirname, 'views'),
+        '@scss': path.resolve(__dirname, 'static/scss'),
+        '@src': path.resolve(__dirname, 'static/js'),
+      },
+    },
+    output: {
+      filename: `static/brakkit${isDev ? '' : '.min'}.js`,
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      ...plugins,
+      new MiniCssExtractPlugin({
+        filename: `static/brakkit${isDev ? '' : '.min'}.css`
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: path.resolve(__dirname, 'static/images/'), to: 'static/images/' }
+        ],
+      }),
+    ],
+  };
 }
