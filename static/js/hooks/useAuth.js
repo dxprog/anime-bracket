78e3251656407ae3d5af36b9eb9ff99ec5bc3c78
Matt Hackmann <matt.hackmann@gmail.com>
2022-10-06 19:58:24 -0700

    created auth context hook

---
+++ static/js/hooks/useAuth.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+
+const ANON_USER_ID = 0;
+
+const AuthContext = React.createContext();
+export const AuthContextProvider = AuthContext.Provider;
+export const useAuth = () => {
+  const context = useContext(AuthContext.Consumer);
+  const userId = context?.userId ? parseInt(context.userId, 10) : ANON_USER_ID;
+
+  return {
+    isAuthenticated: userId > ANON_USER_ID,
+    userId,
+    csrfToken: context?.csrfToken,
+  };
+};
