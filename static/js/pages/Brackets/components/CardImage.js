6993239238c72208ce7754566e96e9cc5af46495
Matt Hackmann <matt.hackmann@gmail.com>
2022-10-16 16:48:51 -0700

    debug cleanup

--- static/js/pages/Brackets/components/CardImage.js
+++ static/js/pages/Brackets/components/CardImage.js
@@ -3,33 +3,6 @@ import classnames from 'classnames';
 
 const CardImage = ({ bracket }) => {
   const { cardImage, entrants, name, winner } = bracket;
-//   {{#if cardImage}}
-//   <img src="{{cardImage}}" alt="{{name}}" class="card-image" />
-// {{else}}
-//   {{#if entrants}}
-//       <div class="card-entrants{{#if winner}} has-winner{{/if}}">
-//           <div class="inner">
-//               {{#each entrants}}
-//                   <img src="{{image}}" alt="{{name}}" title="{{name}}" />
-//               {{/each}}
-//           </div>
-//           {{#if winner}}
-//               <div class="winner">
-//                   <img src="{{winner.image}}" alt="{{winner.name}}" />
-//                   <h3>
-//                       {{winner.name}}
-//                       <span>Winner</span>
-//                   </h3>
-//               </div>
-//           {{/if}}
-//       </div>
-//   {{else}}
-//       <div class="card-entrants">
-//           <img src="/static/images/no_card_image.png" alt="no image" class="no-image" />
-//       </div>
-//   {{/if}}
-// {{/if}}
-console.log(bracket);
   return (
     <>
       {cardImage && (
