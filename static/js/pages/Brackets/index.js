fb7beb3ed35177d9065b5e8f8452cf6edc5422ec
Matt Hackmann <matt.hackmann@gmail.com>
2022-10-16 17:55:37 -0700

    basic filtering/pagination

--- static/js/pages/Brackets/index.js
+++ static/js/pages/Brackets/index.js
@@ -3,18 +3,36 @@ import ReactDOM from 'react-dom';
 import { Route } from 'molecule-router';
 
 import BracketCard from './components/BracketCard';
+import useBracketFilters from './useBracketFilters';
 
 const Brackets = ({ brackets, title }) => {
+  const {
+    displayBrackets,
+    setPage,
+    setSearchString,
+    page,
+    numPages,
+  } = useBracketFilters(brackets);
+
+  const handleSearch = term => {
+    setSearchString(term);
+    setPage(0);
+  };
+
   return (
     <>
       <header>
         <h2>{title}</h2>
+        <input onChange={evt => handleSearch(evt.target.value)} />
       </header>
       <ul className="brackets">
-        {brackets.map(bracket => (
+        {displayBrackets.map(bracket => (
           <BracketCard bracket={bracket} />
         ))}
       </ul>
+      <button onClick={() => setPage(page - 1)} disabled={page === 0}>Previous Page</button>
+      <span>{page + 1} of {numPages}</span>
+      <button onClick={() => setPage(page + 1)} disabled={page + 1 === numPages}>Next Page</button>
     </>
   );
 };
